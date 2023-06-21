d82c4c451ff710bc170ada146322d96049818fa1
Jimshin01 <kisslove.msh@gmail.com>
2023-06-21 07:40:26 +0330

    a

--- Internet-final/server/utils/dist/static.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var path = require('path');
-
-var express = require('express');
-
-module.exports.statics = function (app) {
-  app.use(express["static"](path.join(__dirname, '..', 'public')));
-};
